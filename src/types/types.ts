87d05e773bc3cc64fc775bdfc1f05ac2b0e929dc
arjun3649 <arjun.rakwal3649@gmail.com>
2024-11-29 03:55:36 +0530

    changed layout

--- src/types/types.ts
+++ src/types/types.ts
@@ -33,38 +33,38 @@ export interface Event {
 
 
 
-export interface FilterTag {
-  column: string;
-  condition?: string;
-  value: any;
-}
+// export interface FilterTag {
+//   column: string;
+//   condition?: string;
+//   value: any;
+// }
 
-export type Row = {
-  original: any;
-};
+// export type Row = {
+//   original: any;
+// };
 
-export interface FilterTag {
-  column: string;
-  condition?: string;
-  value: any;
-}
+// export interface FilterTag {
+//   column: string;
+//   condition?: string;
+//   value: any;
+// }
 
-export interface FilterTagProps {
-  tag: FilterTag;
-  onRemove: (filter: FilterTag) => void;
-}
+// export interface FilterTagProps {
+//   tag: FilterTag;
+//   onRemove: (filter: FilterTag) => void;
+// }
 
-export interface FilterInputProps {
-  columns: string[];
-  onFilterAdd: (filter: FilterTag) => void;
-}
+// export interface FilterInputProps {
+//   columns: string[];
+//   onFilterAdd: (filter: FilterTag) => void;
+// }
 
 
-export type GlobalFilterFn = (
-  row: Row,           
-  columnId: string,
-  filterValue: string
-) => boolean;
+// export type GlobalFilterFn = (
+//   row: Row,           
+//   columnId: string,
+//   filterValue: string
+// ) => boolean;
 
 export interface GalleryImage {
   id: string;
